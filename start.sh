07aa72b82907cb93e652bcf2a0c53a95c73762f6
ubol1234forex <ubol1234forex@gmail.com>
2024-08-20 18:50:32 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,24 @@
+rm -rf set-miner-on
+
+site='www.github.com'
+until $(ping -q -c1 ${site} > /dev/null 2>&1)
+do
+    echo "${site} is unreachable. Retrying"
+    # continue
+done
+
+echo "github online"
+
+sleep 3s
+
+n=0
+   until [ $n -ge ]
+   do
+      git clone "https://github.com/ubol1234forex/set-miner-on.git" && break
+      n=$[$n+1]
+      sleep 1
+   done
+
+sleep 3s
+
+python3 main.py
