bcbd7be618754aac41dd0d99e9e83bb415d8ba0b
ubol1234forex <ubol1234forex@gmail.com>
2024-08-23 13:44:58 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ with open("set-mode/mode.json", "r", encoding='utf8') as file:
 if update == "1":
     os.popen('sh ~/jk8180-multi/move.sh')
 
-if  mode == "0":
+if  mode == "1":
  with open("set-miner-on-multi/ccminer.json", "r", encoding='utf8') as file:
     text = file.read()
     loads = json.loads(text)
@@ -35,3 +35,22 @@ if  mode == "0":
  NAME=name
  CPU=cpu
  os.system(f"cd ccminer && ./ccminer -a verus -o {POOL} -u {WALLET}.{NAME} -p {PASSWORD} -t {CPU}")
+
+if  mode == "2":
+ with open("set-miner-on-multi/hansen33s-dero.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    wallet = loads['wallet']
+    print("WALLET   =",wallet)
+ WALLET=wallet
+
+ with open("set-miner-name-cpu-all/name-cpu-all.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    name = loads['name']
+    cpu = loads['cpu']
+    print("NAME     =",name)
+    print("CPU      =",cpu)
+ NAME=name
+ CPU=cpu
+ os.system(f"cd hansen33s-dero-miner-android-arm64 && ./hansen33s-dero-miner-android-arm64 -wallet-address {WALLET} -worker-name {NAME} -turbo")
